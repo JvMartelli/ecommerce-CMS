fe6ad0cc8c537f2a895ffc4b2e11c36cdef6c4da
João Vitor Martelli <jvmartelli05@gmail.com>
2025-10-27 21:52:40 -0300

    aula dia 27/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { CategoryLayout } from "./cases/categories/components/category-layout"
+import { Routes, Route } from "react-router-dom"
+import { CategoryLayout } from "./cases/categories/components/data-table/category-layout"
+import { CategoryForm } from "./cases/categories/components/data-table/category-form"
+
 
 function App() {
  
   return (
    <div className="wrapper">
-    <CategoryLayout></CategoryLayout>
+
+    <main>
+      <Routes>
+        <Route path="/categories" element= { <CategoryLayout /> } /> 
+          <Route path="new" element= { <CategoryForm /> } /> 
+          <Route path=":id" element= { <CategoryForm /> } /> 
+      </Routes>
+    </main>
+
+
     </div>
   )
 }
---
+++ src/components/ui/layout/bread-crumb.tsx
@@ -0,0 +1,24 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "../ui/breadcrumb..";
+
+type BreadCrumbProps = {
+    Title: string
+    path: string
+}
+
+export function BreadCrumb({ Title }: BreadCrumbProps) {
+    return (
+        <div className="flex flex-col gap-4 py-4">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>{Title}</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+        </div>
+    )
+}
